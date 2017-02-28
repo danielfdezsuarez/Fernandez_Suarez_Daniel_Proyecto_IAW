f106553057f7f5e073fa9435547b415cc9407f8f
danielfdezsuarez <danifer91_15@hotmail.com>
2017-02-28 19:47:13 +0100

    nueva bd

--- camisetas.sql
+++ camisetas.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 23-02-2017 a las 14:20:04
--- Versión del servidor: 5.6.33
--- Versión de PHP: 5.6.26
+-- Tiempo de generación: 28-02-2017 a las 19:46:06
+-- Versión del servidor: 5.6.34
+-- Versión de PHP: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -61,29 +61,40 @@ CREATE TABLE `camiseta` (
 --
 
 INSERT INTO `camiseta` (`id_camiseta`, `jugador`, `dorsal`, `marca`, `publicidad`, `temporada`, `competicion`, `imagen`, `observaciones`) VALUES
-(1, NULL, NULL, 'Reebok', 'Fairmont Homes', '2005-06', 'Liga Australia', 'link', NULL),
-(2, NULL, NULL, 'Puma', 'Diners Club, LG', '2007-08', 'liga griega', 'link', NULL),
-(3, NULL, NULL, 'Adidas', 'ABN-AMRO', '2000-01', 'liga holandesa', 'link', NULL),
-(6, 'Eriksen', 8, 'Adidas', 'Aegon', '2012-13', 'liga holandesa', 'link', NULL),
-(83, 'asd', 88, 'af', 'fasf', 'asfa', 'asf', 'img/bnghrr - copia (2).png', ''),
-(87, 'nadal rafa', 10, '', '', '20187', 'australia', 'img/bnghrr - copia - copia - copia.png', ''),
-(89, 'asf', 88, 'saf', 'asf', 'asfa', 'asf', 'img/bnghrr - copia.png', ''),
-(90, 'asf', 88, 'nosa', 'safgfa', 'asfa', 'asf', 'img/badfg.png', ''),
-(96, 'nadal', 11, 'New Balance', 'mallorca', '2017', 'liga española', 'img/camiseta-sevilla-1 - copia - copia.jpg', ''),
-(222, 'Schweinsteiger', 7, 'Adidas', NULL, '2008', 'Euro 2008', 'link', NULL),
-(478, '', 88, '', '', '', '', 'img/map-of-the-lost-island.jpg', ''),
-(481, NULL, NULL, NULL, NULL, NULL, NULL, 'link', NULL),
-(645, 'nadal', 2, '', '', '2015-16', 'liga española', 'img/camiseta-sevilla-2 - copia.jpg', ''),
-(646, 'silvio', 78, '', '', '', '', 'img/seta - copia (3).png', ''),
-(647, 'php', 77, '', '', '', '', 'img/seta - copia (3) - copia.png', ''),
-(648, 'manual', 65, '', '', '', '', 'img/seta - copia (2) - copia.png', ''),
-(649, 'dsfhsfffffffrert', NULL, '', '', '', '', 'img/badfg - copia - copia.png', ''),
-(650, 'yafunciona', NULL, '', '', '', '', 'img/setis.png', ''),
-(651, 'queva', NULL, '', '', '', '', 'img/setis - copia.png', ''),
-(652, 'queva13', NULL, '', '', '', '', 'img/setis - copia - copia.png', ''),
-(653, 'penis', NULL, '', '', '', '', 'img/setag.png', ''),
-(654, 'wwe', NULL, 'nike', '', '', '', 'img/cowboys - copia.png', ''),
-(656, 'paquito', 23, 'adidas', '', '2009-10', 'mundial 2010', 'img/espana3.jpg', '');
+(1, '', NULL, 'joma', '', '2001-02', 'liga_española', 'img/sevilla_1.jpg', 'blanca'),
+(2, '', NULL, 'joma', '', '2001-02', 'liga_española', 'img/sevilla_2.jpg', 'azul'),
+(3, '', NULL, 'joma', '', '2001-02', 'liga_española', 'img/sevilla_3.jpg', 'portero, autografo notario'),
+(4, 'julio_baptista', 10, '', '', '2004-05', 'liga_española', 'img/sevilla_4_julio_baptista_10.jpg', ''),
+(5, '', NULL, 'adidas', 'abn_amro', '2001-02', 'liga_holandesa', 'img/ajax_2.jpg', ''),
+(6, '', NULL, '', 'abn_amro', '2004-05', 'liga_holandesa', 'img/ajax_3.jpg', ''),
+(7, 'dani', 11, 'umbro', 'alpine', '2002-03', 'liga_española', 'img/alaves_1_dani_11.jpg', ''),
+(8, 'schweinsteiger', 7, 'adidas', '', '2008', 'eurocopa_2008', 'img/alemania_1_schweinsteiger_7.jpg', ''),
+(9, 'muller', 13, 'adidas', '', '2010', 'mundial_2010', 'img/alemania_2_muller_13.jpg', ''),
+(10, 'aimar', 16, 'adidas', '', '2002', 'mundial_2002', 'img/argentina_1_aimar_16.jpg', ''),
+(11, '', NULL, 'adidas', '', '2009-10', 'mundial_2010_clasificacion', 'img/argentina_3.jpg', ''),
+(12, '', NULL, 'hummel', '', '2009-10', 'mundial_2010_clasificacion', 'img/armenia.jpg', ''),
+(13, 'henry', 14, '', 'o2', '2004-05', 'liga_inglesa', 'img/arsenal_3_henry_14.jpg', ''),
+(14, '', NULL, 'nike', 'o2', '2004-05', 'liga_inglesa', 'img/arsenal_4.jpg', ''),
+(15, '', NULL, 'nike', '32red.com', '2007-08', 'liga_inglesa', 'img/aston_villa_1.jpg', ''),
+(16, '', NULL, 'reebok', '', '1999-00', 'liga_española', 'img/atletico_madrid_1.jpg', ''),
+(17, '', NULL, 'nike', '', '2002-03', 'liga_española', 'img/barcelona_1.jpg', ''),
+(18, 'dani_alves', 20, 'nike', 'unicef', '2008-09', 'liga_española', 'img/barcelona_3_dani_alves_ 20.jpg', 'sextete'),
+(19, 'ibrahimovic', 9, 'nike', 'unicef', '2009-10', 'liga_española', 'img/barcelona_4_ibrahimovic_9.jpg', 'tv3'),
+(20, 'messi', 10, 'nike', 'unicef', '2010-11', 'liga_española', 'img/barcelona_5_messi_10.jpg', 'campeon_clubes'),
+(21, '', NULL, 'errea', '', '2010-11', 'liga_italiana', 'img/bari.jpg', ''),
+(22, '', NULL, '', 'rwe', '2004-06', 'liga_alemana', 'img/bayer_leverkusen_1.jpg', ''),
+(23, '', NULL, 'adidas', 't_com', '2004-05', 'liga_alemana', 'img/bayern_munich_1.jpg', ''),
+(24, '', NULL, 'adidas', 'vodafone', '2004-05', 'liga_portuguesa', 'img/benfica_1.gif', 'centenario'),
+(25, '', NULL, 'adidas', 'vodafone', '2004-05', 'liga_portuguesa', 'img/benfica_2.gif', 'centenario'),
+(26, 'holosko', 23, 'umbro', 'cola_turka', '2008-09', 'liga_turca', 'img/besiktas_1_holosko_23.jpg', 'avea'),
+(27, '', NULL, 'kappa', 'el_monte', '2005', 'copa_del_rey_2004-05', 'img/betis_1.jpg', 'final_copa_rey'),
+(28, '', NULL, 'nike', 'pepsi', '2003-04', 'liga_argentina', 'img/boca_juniors_1.jpg', ''),
+(29, '', NULL, 'nike', 'megatone', '2008-09', 'liga_argentina', 'img/boca_juniors_2.jpg', ''),
+(30, '', NULL, 'reebok', 'rbk', '2006-07', 'liga_inglesa', 'img/bolton_wanderers_1.jpg', ''),
+(31, '', NULL, 'nike', 'e_on', '2004-05', 'liga_alemana', 'img/borussia_dortmund_1.jpg', ''),
+(32, 'ronaldinho', 10, 'nike', '', '2004', 'copa_america_2004', 'img/brasil_1_ronaldinho_10.jpg', ''),
+(33, 'neymar', 11, 'nike', '', '2011', 'copa_america_2011', 'img/brasil_2_neymar_11.jpg', ''),
+(34, '', NULL, 'puma', '', '2004', 'eurocopa_2004', 'img/bulgaria_1.jpg', '');
 
 -- --------------------------------------------------------
 
@@ -102,23 +113,39 @@ CREATE TABLE `camiseta_equipo` (
 
 INSERT INTO `camiseta_equipo` (`id_camiseta`, `id_equipo`) VALUES
 (1, 1),
-(648, 1),
-(649, 1),
-(650, 1),
-(651, 1),
-(652, 1),
-(653, 1),
-(2, 2),
-(90, 2),
-(3, 3),
-(6, 3),
-(87, 4),
-(478, 4),
-(481, 4),
-(83, 5),
-(222, 5),
-(654, 79),
-(656, 80);
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 2),
+(6, 2),
+(7, 3),
+(8, 4),
+(9, 4),
+(10, 5),
+(11, 5),
+(12, 6),
+(13, 7),
+(14, 7),
+(15, 8),
+(16, 9),
+(17, 10),
+(18, 10),
+(19, 10),
+(20, 10),
+(21, 11),
+(22, 12),
+(23, 13),
+(24, 14),
+(25, 14),
+(26, 15),
+(27, 16),
+(28, 17),
+(29, 17),
+(30, 18),
+(31, 19),
+(32, 20),
+(33, 20),
+(34, 21);
 
 -- --------------------------------------------------------
 
@@ -140,21 +167,27 @@ CREATE TABLE `equipo` (
 --
 
 INSERT INTO `equipo` (`id_equipo`, `club_seleccion`, `nombre`, `pais`, `continente`, `imagen_equipo`) VALUES
-(1, 'club', 'adelaide united', 'australia', 'oceania', 'link2'),
-(2, 'club', 'pao', 'grecia', 'europa', 'img/escudo.jpg'),
-(3, '', 'ajax', 'holanda', 'europa', 'link2'),
-(4, 'seleccion', 'albania', 'albania', 'europa', 'link2'),
-(5, 'seleccion', 'alemania', 'alemania', 'europa', 'link2'),
-(8, '', '', '', '', 'img/logo_left.png'),
-(9, '', '', '', '', 'img/bnghrr.png'),
-(11, 'club', '', '', '', 'img/icono_equipo.jpg'),
-(12, '', '', '', '', 'img/icono_equipo - copia.jpg'),
-(13, 'club', '', '', '', 'img/logo_left - copia.png'),
-(16, 'club', '', '', '', 'img/logo_left - copia (2).png'),
-(78, 'club', 'asf', 'aff', 'asdg', 'img/logo_left - copia (3).png'),
-(79, 'seleccion', 'mibeti2', 'aki', 'er mundo', 'img/cowboys.png'),
-(80, 'seleccion', 'españa', 'españa', 'europa', 'img/espana2.jpg'),
-(81, 'club', 'cosa', 'asf', 'dsagf', 'img/pia.png');
+(1, 'club', 'sevilla_fc', 'españa', 'europa', 'img/escudo_sevilla1.png'),
+(2, 'club', 'ajax', 'holanda', 'europa', 'img/escudo_ajax1.png'),
+(3, 'club', 'alaves', 'españa', 'europa', 'img/escudo_alaves1.png'),
+(4, 'seleccion', 'alemania', 'alemania', 'europa', 'img/escudo_alemania1.png'),
+(5, 'seleccion', 'argentina', 'argentina', 'america_del_sur', 'img/escudo_argentina1.png'),
+(6, 'seleccion', 'armenia', 'armenia', 'europa', 'img/escudo_armenia1.png'),
+(7, 'club', 'arsenal_fc', 'inglaterra', 'europa', 'img/escudo_arsenal1.png'),
+(8, 'club', 'aston_villa', 'inglaterra', 'europa', 'img/escudo_aston_villa1.png'),
+(9, 'club', 'atletico_madrid', 'españa', 'europa', 'img/escudo_atletico_madrid1.png'),
+(10, 'club', 'barcelona', 'españa', 'europa', 'img/escudo_barcelona1.png'),
+(11, 'club', 'as_bari', 'italia', 'europa', 'img/escudo_bari1.png'),
+(12, 'club', 'bayer_leverkusen', 'alemania', 'europa', 'img/escudo_bayer_leverkusen1.png'),
+(13, 'club', 'bayern_munich', 'alemania', 'europa', 'img/escudo_bayern_munich1.png'),
+(14, 'club', 'benfica', 'portugal', 'europa', 'img/escudo_benfica1.png'),
+(15, 'club', 'besiktas', 'turquia', 'europa', 'img/escudo_besiktas1.jpg'),
+(16, 'club', 'real_betis', 'españa', 'europa', 'img/escudo_betis1.png'),
+(17, 'club', 'boca_juniors', 'argentina', 'america_del_sur', 'img/escudo_boca_juniors1.png'),
+(18, 'club', 'bolton_wanderers', 'inglaterra', 'europa', 'img/escudo_bolton1.png'),
+(19, 'club', 'borussia_dortmund', 'alemania', 'europa', 'img/escudo_borussia_dortmund1.png'),
+(20, 'seleccion', 'brasil', 'brasil', 'america_del_sur', 'img/escudo_brasil1.png'),
+(21, 'seleccion', 'bulgaria', 'bulgaria', 'europa', 'img/escudo_bulgaria1.png');
 
 -- --------------------------------------------------------
 
@@ -220,12 +253,12 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `camiseta`
 --
 ALTER TABLE `camiseta`
-  MODIFY `id_camiseta` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=659;
+  MODIFY `id_camiseta` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 --
 -- AUTO_INCREMENT de la tabla `equipo`
 --
 ALTER TABLE `equipo`
-  MODIFY `id_equipo` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+  MODIFY `id_equipo` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 --
 -- Restricciones para tablas volcadas
 --
